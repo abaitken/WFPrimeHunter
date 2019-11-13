908ff8cf86336e6f372082a3e0509525b8ba5519
Alex Boyne-Aitken <abaitken@live.co.uk>
2019-11-13 15:15:03 +0000

    Introduced paged views to improve performance

--- src/components/inventory.js
+++ src/components/inventory.js
@@ -7,17 +7,56 @@ function PagedItems(ko) {
 
     self.items = ko.observableArray();
     self.pageIndex = ko.observable(0);
-    self.pageCount = ko.computed(function() {
+    self.pageCount = ko.computed(function () {
         var result = Math.floor(self.items().length / PAGE_SIZE);
         result += self.items().length % PAGE_SIZE > 0 ? 1 : 0;
         return result - 1;
     });
-    
-    self.pagedItems = ko.computed(function() {
+    self.pages = ko.computed(function () {
+        let result = [];
+        for (let index = 0; index <= self.pageCount(); index++) {
+            result.push(index + 1);
+        }
+        return result;
+    });
+
+    self.gotoPage = function (pageNumber) {
+        self.pageIndex(pageNumber - 1);
+    };
+
+    self.pagedItems = ko.computed(function () {
         var startIndex = self.pageIndex() * PAGE_SIZE;
         return self.items.slice(startIndex, startIndex + PAGE_SIZE);
     });
 
+    self.hasPrevious = ko.computed(function () {
+        return self.pageIndex() !== 0;
+    });
+
+    self.hasNext = ko.computed(function () {
+        return self.pageIndex() !== self.pageCount();
+    });
+
+    self.next = function () {
+        if (self.pageIndex() < self.pageCount()) {
+            self.pageIndex(self.pageIndex() + 1);
+        }
+    }
+
+    self.previous = function () {
+        if (self.pageIndex() != 0) {
+            self.pageIndex(self.pageIndex() - 1);
+        }
+    }
+
+    self.addItem = function (item) {
+        self.items.push(item);
+    };
+
+    self.removeItem = function (item) {
+        var index = self.items().indexOf(item);
+        self.items.splice(index, 1);
+    };
 };
 
 module.exports = function (ko, $) {
@@ -28,17 +67,18 @@ module.exports = function (ko, $) {
             self.required = new PagedItems(ko);
 
             self._moveItem = function (item, from, to) {
-                var index = from.items().indexOf(item);
-                from.items.splice(index, 1);
-                to.items.push(item);
+                from.removeItem(item);
+                to.addItem(item);
             };
 
-            self.itemAcquired = function (item) {
-                self._moveItem(item, self.required, self.acquired);
+            self.itemAcquired = function (item) {                
+                self.required.removeItem(item);
+                self.acquired.addItem(item);
             };
 
             self.itemLost = function (item) {
-                self._moveItem(item, self.acquired, self.required);
+                self.acquired.removeItem(item);
+                self.required.addItem(item);
             };
 
             self.init = function () {
