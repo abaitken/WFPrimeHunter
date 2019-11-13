e68d1137241dc285c47cd13e4eb90a4f5e3864e0
Alex Boyne-Aitken <abaitken@live.co.uk>
2019-11-13 14:59:22 +0000

    Efforts towards improving performance

--- src/components/inventory.js
+++ src/components/inventory.js
@@ -1,11 +1,45 @@
 const view = require("./inventory.html");
 
+const PAGE_SIZE = 25;
+
+function PagedItems(ko) {
+    var self = this;
+
+    self.items = ko.observableArray();
+    self.pageIndex = ko.observable(0);
+    self.pageCount = ko.computed(function() {
+        var result = Math.floor(self.items().length / PAGE_SIZE);
+        result += self.items().length % PAGE_SIZE > 0 ? 1 : 0;
+        return result - 1;
+    });
+    
+    self.pagedItems = ko.computed(function() {
+        var startIndex = self.pageIndex() * PAGE_SIZE;
+        return self.items.slice(startIndex, startIndex + PAGE_SIZE);
+    });
+
+};
 
 module.exports = function (ko, $) {
     return {
         viewModel: function (root) {
             var self = this;
-            self.items = ko.observableArray();
+            self.acquired = new PagedItems(ko);
+            self.required = new PagedItems(ko);
+
+            self._moveItem = function (item, from, to) {
+                var index = from.items().indexOf(item);
+                from.items.splice(index, 1);
+                to.items.push(item);
+            };
+
+            self.itemAcquired = function (item) {
+                self._moveItem(item, self.required, self.acquired);
+            };
+
+            self.itemLost = function (item) {
+                self._moveItem(item, self.acquired, self.required);
+            };
 
             self.init = function () {
 
@@ -22,14 +56,13 @@ module.exports = function (ko, $) {
                             const name = data.items[data.itemids[index]];
                             list.push({
                                 id: data.itemids[index],
-                                name: name,
-                                owned: false
+                                name: name
                             });
                         }
-                        list.sort(function(a, b){
+                        list.sort(function (a, b) {
                             return a.name > b.name;
                         });
-                        self.items(list);
+                        self.required.items(list);
                     })
                     .fail(function (jqXHR, textStatus, errorThrown) {
                         root.errors.error(errorThrown);
